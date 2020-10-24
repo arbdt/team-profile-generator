2f66f5f38947ba45432e636f27bc55df79b01beb
arbdt <67365149+arbdt@users.noreply.github.com>
2020-10-24 19:42:28 +1030

    updated Employee class file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,26 +1,32 @@
-// TODO: Write code to define and export the Employee class
+// Employee base class definition
 class Employee {
+    //constructor
     constructor(name, id, email){
-        this.name = name,
-        this.id = id,
-        this.email = email
+        this.name = name; //set employee name
+        this.id = id; // set employee ID
+        this.email = email; // set employee email
     }
 
+    // function to get name
     getName() {
         return this.name;
     }
     
+    //function to get ID
     getId() {
         return this.id;
     }
     
+    //function to get email
     getEmail() {
         return this.email;
     }
     
+    //function to get role
     getRole() {
         return "Employee";
     }
 }
 
+// make usable in other files
 module.exports = Employee;
