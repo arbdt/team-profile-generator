7044a8f6d139456dc34207facfa8a712c1977c0e
arbdt <67365149+arbdt@users.noreply.github.com>
2020-10-25 10:02:57 +1030

    added Engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,27 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// Engineer subclass definition
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        //call Employee constructor on basic parameters
+        super(name, id, email);
+
+        // set class-specific parameter
+        this.github = github; 
+      
+    }
+    // class-specific methods
+
+    // function to get github ID
+    getGithub() {
+        return this.github;
+    }
+    
+    // function to get role
+    getRole() {
+        return "Engineer";
+    }
+}
+
+// make usable in other files
+module.exports = Engineer;
